f2c50305e286101e1d34377ffcec66cd9f7eb22b
Muhammed4mach <moha4mach@gmail.com>
2024-03-05 07:58:31 +0200

    Added Docs

--- enums/syntaxkind.h
+++ enums/syntaxkind.h
@@ -1,15 +1,57 @@
 #pragma once
 
+/**
+ * Description
+ */
 enum SyntaxKind {
+    /**
+     * Description
+     */
     NumberToken,
+
+    /**
+     * Description
+     */
     WhiteSpaceToken,
+
+    /**
+     * Description
+     */
     PlusToken,
+
+    /**
+     * Description
+     */
     MinusToken,
+
+    /**
+     * Description
+     */
     StarToken,
+
+    /**
+     * Description
+     */
     SlashToken,
+
+    /**
+     * Description
+     */
     OpenParenthesisToken,
+
+    /**
+     * Description
+     */
     CloseParenthesisToken,
+
+    /**
+     * Description
+     */
     EndOfLineToken,
+
+    /**
+     * Description
+     */
     BadToken
 };
 
--- types/value.h
+++ types/value.h
@@ -1,10 +1,28 @@
 #pragma once
 #include <string>
 
+/**
+ * Description
+ */
 struct Value {
+    /**
+     * Description
+     */
     int *pInt = nullptr;
+
+    /**
+     * Description
+     */
     double *pDouble = nullptr;
+
+    /**
+     * Description
+     */
     char *pChar = nullptr;
+
+    /**
+     * Description
+     */
     std::string *pString = nullptr;
 };
 
